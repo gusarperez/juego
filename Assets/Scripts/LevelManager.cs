1124cb5faaa261e9f88af46529d267ec9d32af77
gusarperez <35226932+gusarperez@users.noreply.github.com>
2018-03-11 23:16:47 -0600

    juego

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;		// Requiered to switch scenes
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+    
+    public void NextLevel()
+    {
+        //
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+	public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+		//	Application.LoadLevel (name);    -- This method was deprecated a long time ago
+		SceneManager.LoadScene ("Level_01");
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+}
